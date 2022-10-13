e7a95df61d8302b39e05d13a8b1bcea61f104121
Sophia de Jong <Sophia@Sophia-Marina-de-Jong-3.local>
2022-10-13 17:33:36 +0200

    detailsnotworking

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,63 @@
 <template>
-  
+ <Navbar />
+  <div class="container-box">
+    <div class="left">
+      <CountriesList :countries="countriesListReduced"> </CountriesList>
+    </div>
+    <div class="right">
+      <CountryDetails></CountryDetails>
+    </div>
+   </div>
+   <router-view />
 </template>
-  
-<script setup>
-   
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import CountriesList from './views/CountriesList.vue';
+import CountryDetails from "./views/CountryDetails.vue";
+import json from "./assets/countries.json";
+
+export default {
+  data() {
+    return {
+      countriesJson: json,
+    };
+  },
+  computed: {
+    countriesListReduced() {
+      let id = 0;
+      let urlRoute = "https://flagpedia.net/data/flags/icon/72x54/";
+      return this.countriesJson.map((countries) => {
+        return {
+          countryId: id++,
+          countryName: countries.name.official,
+          countryAlpha2Code: countries.alpha2Code.toLowerCase(),
+          countryAlpha3Code: countries.alpha3Code,
+          countryImageURL:
+            urlRoute + countries.alpha2Code.toLowerCase() + ".png",
+        };
+      });
+    },
+  },
+  components: { Navbar, CountriesList, CountryDetails },
+};
 </script>
-  
+
 <style scoped>
-  
+.container-box {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+}
+.left {
+  width: 40%;
+  height: 100%;
+  text-align: center,
+}
+.right {
+  width: 60%;
+   height: 100%;
+  text-align: center,
+}
+
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+    <!-- <h1>Choose a country</h1> -->
+</template>
+<script setup></script>
+<style scoped></style>
